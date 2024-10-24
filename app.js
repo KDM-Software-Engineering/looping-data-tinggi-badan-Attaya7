41df26bd1ccb79faa795b1f69fff624a3c8e83f2
Aryasatya Fitradhia Attaya <attayabtg@gmail.com>
2024-10-24 14:39:03 +0700

    selesa

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 function runner() {
 
   // TULIS SCRIPT KAMU DI SINI
-
-
-
-
-
-
+  const runner = [["Rizki","Ambar","Rada","Tika","Malik","Nanang","Azizah"],[150, 155, 148, 165, 170, 168, 166]];
+  for (let i=0 ; i < 8 ; i++){
+      console.log(`Atlet ${runner[0][i]} tinggi badannya ${runner[1][i]}cm`);
+  }
   /// JANGAN UBAH SELAINNYA
 }
 
